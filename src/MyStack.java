5a4acb67db9efa5235dc1a4bcb6fb652e42185a4
Russ <100997420+sharp-coderr@users.noreply.github.com>
2025-04-18 21:24:53 +0500

    Implement MyStack

---
+++ src/MyStack.java
@@ -0,0 +1,43 @@
+﻿import java.util.Iterator;
+
+public class MyStack<T extends Comparable<T>>{
+
+    private MyLinkedList<T> list = new MyLinkedList<>();
+
+    public void push(T item) {
+        list.addLast(item);
+    }
+
+    public T pop() {
+        if (list.size() == 0) throw new IllegalStateException("Stack is empty");
+        T top = list.getLast();
+        list.removeLast();
+        return top;
+    }
+
+    public T peek() {
+        if (list.size() == 0) throw new IllegalStateException("Stack is empty");
+        return list.getLast();
+    }
+
+    public boolean isEmpty() {
+        return list.size() == 0;
+    }
+
+
+    public T[] toArray() {
+        return list.toArray();
+    }
+
+    public void clear() {
+        list.clear();
+    }
+
+    public int size() {
+        return list.size();
+    }
+
+    public Iterator<T> iterator() {
+        return list.iterator();
+    }
+}
